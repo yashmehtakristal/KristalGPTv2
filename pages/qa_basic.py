0c2ebd1fb783d7bfd8bc727fa786fd115f202825
yashmehtakristal <144411620+yashmehtakristal@users.noreply.github.com>
2023-11-10 02:18:39 +0800

    Update qa_basic.py

--- pages/qa_basic.py
+++ pages/qa_basic.py
@@ -32,6 +32,8 @@ if not openai_api_key:
         "Please check with creator of the program (OpenAI keys can be found at https://platform.openai.com/account/api-keys)"
     )
 
+st.session_state.username = st.session_state.username
+
 def change_states():
     st.session_state.logged_out = True
     st.session_state.logged_in = False
