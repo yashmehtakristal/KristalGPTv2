d882ca698a9c49a659afc4ad9d848cb3f43ce2c5
yashmehtakristal <144411620+yashmehtakristal@users.noreply.github.com>
2023-11-09 23:31:24 +0800

    Update main.py

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ if st.session_state.logged_out:
     hide_pages(["About", "Bulk Upload - Basic", "Bulk Upload - Advanced", "Q&A - Basic", "Q&A - Advanced"])
 
 if "username" not in st.session_state:
-    st.session_state['username'] = None
+    st.session_state['username'] = ''
 
 if "Authenticator" not in st.session_state:
     st.session_state['Authenticator'] = None
@@ -57,18 +57,18 @@ def login_button_pressed():
     
     info, info1 = st.columns(2)
     
-    if st.session_state['username']:
-        if st.session_state['username'] in usernames:
+    if username:
+        if username in usernames:
             if password:
 
                 st.session_state.username = username
-                password_match = bcrypt.checkpw(password.encode(), credentials['usernames'][st.session_state['username']]['password'].encode())
+                password_match = bcrypt.checkpw(password.encode(), credentials['usernames'][username]['password'].encode())
 
                 if password_match is True:
                     st.session_state.logged_in = True
                     st.session_state.logout = False
 
-                    st.sidebar.subheader('Welcome', st.session_state['username'])
+                    st.sidebar.subheader(f'Welcome {username}')
                     logout_button = Authenticator.logout('Log Out', 'sidebar')
 
                     if logout_button:
@@ -128,8 +128,6 @@ try:
                         2) Username contains only alphanumeric characters (letters and digits)
                         '''
                         )
-        
-        st.session_state['username'] = username
         
         password = st.text_input('Password', placeholder='Enter Your Password', type='password',
                             help =
