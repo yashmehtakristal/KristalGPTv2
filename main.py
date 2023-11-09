b8f479ff1033a49c60cdd8ae0ba448c7cd97be76
yashmehtakristal <144411620+yashmehtakristal@users.noreply.github.com>
2023-11-09 23:51:59 +0800

    Update main.py

--- main.py
+++ main.py
@@ -72,8 +72,6 @@ def login_button_pressed(username, password):
                         st.session_state.logged_out = True
                         st.session_state.logged_in = False
 
-                    st.rerun()
-
                 elif password_match is False:
                     with info:
                         st.error('Incorrect Password or username')
